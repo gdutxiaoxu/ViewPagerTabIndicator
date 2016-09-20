1d9ea3e20a367ad7da765197e83718c44082417f
973243764@qq.com <xujun410250828>
2016-09-20 14:06:39 +0800

    增加ToolBar例子

--- tagindicatordemo/src/main/java/com/xujun/administrator/tagindicatordemo/MainActivity.java
+++ tagindicatordemo/src/main/java/com/xujun/administrator/tagindicatordemo/MainActivity.java
@@ -2,7 +2,6 @@ package com.xujun.administrator.tagindicatordemo;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.xujun.administrator.tagindicatordemo.base.BaseActivity;
 
@@ -25,9 +24,10 @@ public class MainActivity extends BaseActivity {
     public void onButtonClick(View view) {
         switch (view.getId()) {
             case R.id.btn_test:
-                Toast.makeText(MainActivity.this, "该功能尚未更新，有兴趣的请关注该项目，待带更新", Toast.LENGTH_SHORT)
-                        .show();
-                //  readyGo(GuideActivity.class);
+                //                Toast.makeText(MainActivity.this, "该功能尚未更新，有兴趣的请关注该项目，待带更新",
+                // Toast.LENGTH_SHORT)
+                //                        .show();
+                readyGo(ToolBarActivity.class);
                 break;
             case R.id.btn_pagIndicator:
                 readyGo(PagerIndicatorActivity.class);
---
+++ tagindicatordemo/src/main/java/com/xujun/administrator/tagindicatordemo/ToolBarActivity.java
@@ -0,0 +1,12 @@
+package com.xujun.administrator.tagindicatordemo;
+
+import com.xujun.administrator.tagindicatordemo.base.BaseActivity;
+
+public class ToolBarActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.activity_tool_bar;
+    }
+}
